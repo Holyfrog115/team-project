6caab94132be696e6e1935c0ca54cafb4e83cec9
Egor Konyshev <149291415+Holyfrog115@users.noreply.github.com>
2026-09-25 15:37:51 +0700

    Создан файл реализации, добавлены функции перевода

---
+++ konyshev.cpp
@@ -0,0 +1,14 @@
+#include "konyshev.h"
+#include <cmath>
+
+// Перевод километры -> мили
+double kmToMiles(double km)
+{
+    return km * 0.621371;
+}
+
+// Перевод мили -> километры
+double milesToKm(double mi)
+{
+    return mi * 1.609344;
+}
